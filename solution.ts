3c4eb68a787a144afcd749c59a9a0aaf33a1cd63
newajDev <newaj.design@gmail.com>
2025-11-18 05:21:55 +0600

    Solution Done

--- solution.ts
+++ solution.ts
@@ -1,6 +1,5 @@
-//problem 01
-type valueOfFormatValue = string | number | boolean;
-function formatValue(value: valueOfFormatValue): valueOfFormatValue {
+type TvalueOfFormatValue = string | number | boolean;
+function formatValue(value: TvalueOfFormatValue): TvalueOfFormatValue {
   if (typeof value === "string") {
     return value.toUpperCase();
   } else if (typeof value === "number") {
@@ -9,65 +8,59 @@ function formatValue(value: valueOfFormatValue): valueOfFormatValue {
     return !value;
   }
 }
-// problem 02
 
-function getLength() {
-  
+type TvalueOfLength = string | Array<string | number>;
+function getLength(value: Tlength): number {
+  return value.length;
 }
-// problem 03
+
 class Person {
   constructor(public name: string, public age: number) {}
 
-  getDetails() {
+  getDetails(): string {
     return `'Name: ${this.name}, Age: ${this.age}'`;
   }
 }
 
-
-
-// problem 04
 type TbookObj = {
   title: string;
   rating: number;
 };
-type checkRating<T> = Array<T>
-function filterByRating(books: checkRating<TbookObj>): checkRating<TbookObj> {
+type TcheckRating<T> = Array<T>;
+function filterByRating(books: TcheckRating<TbookObj>): TcheckRating<TbookObj> {
   return books.filter((item) => item.rating <= 5 && item.rating >= 4);
 }
 
-//problem 05
-type Tobject = {
+type TobjectOfArray = {
   id: number;
   name: string;
   email: string;
   isActive: boolean;
 };
-type filterValue<T> = Array<T>;
-function filterActiveUsers(value: filterValue<Tobject>): filterValue<Tobject> {
-  const activeUser: filterValue<Tobject> = [];
+type TfilterValue<T> = Array<T>;
+function filterActiveUsers(
+  value: TfilterValue<TobjectOfArray>
+): TfilterValue<TobjectOfArray> {
+  const activeUser: TfilterValue<TobjectOfArray> = [];
 
   value.forEach((val) => val.isActive === true && activeUser.push(val));
 
   return activeUser;
 }
 
-//problem 06
 interface Book {
   title: string;
   author: string;
   publishedYear: number;
   isAvailable: boolean;
 }
-function printBookDetails(obj: Book) {
-  return console.log(
-    `Title: ${obj.title}, Author: ${obj.author}, Published: ${
-      obj.publishedYear
-    }, Available: ${obj.isAvailable ? "Yes" : "No"}`
-  );
+function printBookDetails(obj: Book): string {
+  return `Title: ${obj.title}, Author: ${obj.author}, Published: ${
+    obj.publishedYear
+  }, Available: ${obj.isAvailable ? "Yes" : "No"}`;
 }
 
-//problem 07
-type TuniqueValue = (string | number)[];
+type TuniqueValue = Array<string | number>;
 function getUniqueValues(
   value1: TuniqueValue,
   value2: TuniqueValue
@@ -83,16 +76,15 @@ function getUniqueValues(
   return removedDublicate;
 }
 
-// problem 08
 type TarrayProperty<T> = Array<T>;
-type objProperty = {
+type TobjProperty = {
   name: string;
   price: number;
   quantity: number;
   discount?: number;
 };
 function calculateTotalPrice(
-  productArray: TarrayProperty<objProperty>
+  productArray: TarrayProperty<TobjProperty>
 ): number {
   return productArray.reduce((total, product) => {
     const { price, quantity, discount = 0 } = product;
