0471b5496b49a43155093b1aaa542b33778aca64
newajDev <newaj.design@gmail.com>
2025-11-17 07:36:24 +0600

    Problem 8:calculateTotalPrice fuction completed

--- solution.ts
+++ solution.ts
@@ -11,3 +11,31 @@ function formatValue(value: valueOfFormatValue): valueOfFormatValue {
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+type TarrayProperty<T> = Array<T>;
+type objProperty = {
+  name: string;
+  price: number;
+  quantity: number;
+  discount?: number;
+};
+
+function calculateTotalPrice(productArray: TarrayProperty<objProperty>) {
+  return productArray.reduce((total, product) => {
+    const { price, quantity, discount = 0 } = product;
+
+    const productTotal = price * quantity * (1 - discount / 100);
+
+    return total + productTotal;
+  }, 0);
+}
